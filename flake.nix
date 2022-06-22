e8c0aeba9a97ff887bb9964ea4eaaa67f2b9a55c
Eric Crosson <eric.s.crosson@utexas.edu>
2022-06-22 07:15:22 -0500

    chore: use nix

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Tap functions";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:ericcrosson/pre-commit-hooks.nix/add-eslint";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        checks = {
+          pre-commit-check = pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              alejandra.enable = true;
+              nix-linter.enable = true;
+              prettier.enable = true;
+              eslint = {
+                enable = true;
+                excludes = ["package-lock.json"];
+              };
+            };
+            settings = {
+              eslint = {
+                extensions = "\\.(js|jsx|ts|tsx|json)$";
+              };
+            };
+          };
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs
+          ];
+          shellHook = ''
+            export PATH="$PWD/node_modules/.bin:$PATH"
+            ${(self.checks.${system}).pre-commit-check.shellHook}
+          '';
+        };
+      }
+    );
+}
